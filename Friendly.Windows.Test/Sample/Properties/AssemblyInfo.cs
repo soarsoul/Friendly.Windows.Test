63a66360daa09b002dc1dcb6cbef19b87105db13
soarsoul <soarsoul@126.com>
2019-08-27 14:08:58 +0800

    initial

---
+++ Friendly.Windows.Test/Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sample")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8aec6905-3b6c-4ede-bde4-305ee8874ac3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Friendly.Windows.Test/Sample/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Codeer.Friendly.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sample
+{
+    [TestClass]
+    public class Test
+    {
+        WindowsAppFriend _app;
+
+        [TestInitialize]
+        void TestInitialize()
+        {
+            //attach to target process!
+            var path = Path.GetFullPath("../../../Target/bin/Debug/Target.exe");
+            _app = new WindowsAppFriend(Process.Start(path));
+        }
+    }
+}
